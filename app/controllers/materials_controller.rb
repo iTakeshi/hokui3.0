5a8f72d60f6727524fd1d6e3f6dea37b61de2024
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-03-29 18:49:23 +0900

    Materials#create

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -8,8 +8,29 @@ class MaterialsController < ApplicationController
     @material = @subject.materials.new(material_type: params[:type])
   end
 
+  def create
+    @material = @subject.materials.new(material_params)
+    @material.user = current_user
+    @material.set_file_params(file_params)
+
+    if @material.save
+      @material.save_file(file_params[:file])
+      redirect_to exams_study_subject_materials_path(@subject)
+    else
+      render action: :new
+    end
+  end
+
   private
   def set_subject
     @subject = Subject.find_by(title_en: params[:subject_title_en])
+  end
+
+  def material_params
+    params.require(:material).permit %i(material_type class_year number with_answer comments)
+  end
+
+  def file_params
+    params.require(:material).permit %i(file file_name)
   end
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,66 @@
 class Material < ActiveRecord::Base
   belongs_to :subject
   belongs_to :user
+
+  def set_file_params(file_params)
+    if file_params[:file_name].blank?
+      self.file_name = file_params[:file].original_filename.split('.').first
+    else
+      self.file_name = file_params[:file_name]
+    end
+    self.file_content_type = file_params[:file].content_type
+  end
+
+  def save_file(file)
+    File.open(Rails.root.join('public', 'uploads', self.internal_file_name), 'wb') do |f|
+      f.write(file.read)
+    end
+  end
+
+  def internal_file_name
+    [self.id.to_s, self.file_ext].join('.')
+  end
+
+  def file_ext
+    case self.file_content_type
+    when 'application/pdf'
+      return 'pdf'
+    when 'image/jpeg'
+      return 'jpg'
+    when 'image/png'
+      return 'png'
+    when 'image/gif'
+      return 'gif'
+    when 'image/tiff'
+      return 'tif'
+    when 'application/msword'
+      return 'doc'
+    when 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+      return 'docx'
+    when 'application/vnd.ms-excel'
+      return 'xls'
+    when 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      return 'xlsx'
+    when 'application/vnd.ms-excel.sheet.macroEnabled.12'
+      return 'xlsm'
+    when 'application/vnd.ms-powerpoint'
+      return 'ppt'
+    when 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+      return 'pptx'
+    when 'text/plain'
+      return 'txt'
+    when 'text/rtf'
+      return 'rtf'
+    when 'text/html'
+      return 'html'
+    when 'application/rtf'
+      return 'rtf'
+    when 'application/x-zip-compressed'
+      return 'zip'
+    when 'application/x-rar-compressed'
+      return 'rar'
+    else
+      return nil
+    end
+  end
 end
