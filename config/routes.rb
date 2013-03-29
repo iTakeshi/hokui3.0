b3f4e94e625f628e0d857bf536c190577db6c973
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-03-29 16:06:54 +0900

    Study#subject

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ HokuiNet::Application.routes.draw do
 
   get  '/study' => 'study#index', as: 'study'
   get  '/study/:subject_title_en' => 'study#subject', as: 'study_subject'
+  get  '/study/:subject_title_en/materials/exams'    => 'materials#exams',    as: 'study_subject_exams'
+  get  '/study/:subject_title_en/materials/quizzes'  => 'materials#quizzes',  as: 'study_subject_quizzes'
+  get  '/study/:subject_title_en/materials/notes'    => 'materials#notes',    as: 'study_subject_notes'
+  get  '/study/:subject_title_en/materials/personal' => 'materials#personal', as: 'study_subject_personal'
 
   namespace :admin do
     resources :users, only: %i(index show) do
