c2894268698fe07ac56da72792169b4a2c57264b
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-03-29 16:29:41 +0900

    Materials#exams(temporary)

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,2 +1,11 @@
 class MaterialsController < ApplicationController
+  before_action :set_subject
+
+  def exams
+  end
+
+  private
+  def set_subject
+    @subject = Subject.find_by(title_en: params[:subject_title_en])
+  end
 end
