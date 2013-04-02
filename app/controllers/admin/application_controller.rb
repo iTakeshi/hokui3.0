0d4de3578bb1c1bb3f2737e96bcb89f9e48cd0e7
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-04-02 15:03:35 +0900

    layouts/admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,9 @@
 class Admin::ApplicationController < ApplicationController
   before_action :authenticate_as_admin
 
+  layout 'layouts/admin'
+
+  private
   def authenticate_as_admin
     redirect_to root_path unless current_user.is_admin
   end
