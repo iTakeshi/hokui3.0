a89d5e435e0e0d906e657a4e9fdd98e128cf64bd
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-04-03 16:33:02 +0900

    generate PasswordResetController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HokuiNet::Application.routes.draw do
+  get "password_reset/new"
   get  '/signup' => 'signup#new'
   post '/signup' => 'signup#create'
   get  '/signup/confirm/:secret_token' => 'signup#confirm', as: 'confirm_signup'
