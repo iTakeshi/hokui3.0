c9cdc220f2badb0c76110b054be5e625cdece0a5
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-04-11 22:27:51 +0900

    gmail fetching

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'nokogiri'
 
 gem 'kaminari'
 
+gem 'gmail'
+gem 'mail', github: 'iTakeshi/mail', branch: 'fix_UndefinedConversionError'
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 # gem 'turbolinks'
 
--- app/models/ml_account.rb
+++ app/models/ml_account.rb
@@ -1,4 +1,44 @@
 class MlAccount < ActiveRecord::Base
   has_many :ml_archives
   belongs_to :year
+
+  def subject_prefix_to_regexp
+    self.subject_prefix.sub('%d', '\d+')
+  end
+
+  def fetch
+    require 'gmail'
+    require 'mail'
+
+    Gmail.new(APP_CONFIG['gmail']['user'], APP_CONFIG['gmail']['pass']) do |gmail|
+      label = "ml#{self.year.class_year}"
+      mails = gmail.label(label).emails(:unread, { to: self.email }).map { |email| Mail.new(email.raw_source) }
+
+      mails.sort {|a, b| a.subject <=> b.subject}.each do |mail|
+      binding.pry
+      mail.subject
+        if self.subject_prefix
+          next if mail.subject !~ %r(#{self.subject_prefix_to_regexp})
+        end
+
+        if mail.multipart?
+          part = mail.all_parts.select { |p| p.content_type =~ %r(text/plain) }.first
+          if part
+            body = part.decoded.to_lf
+          else
+            next
+          end
+        else
+          body = mail.decoded.to_lf
+        end
+
+        self.ml_archives.create!(
+          from: "#{mail.from[0]}",
+          sent_at: "#{mail.date.to_s}",
+          subject: "#{mail.subject.gsub('"', '\"')}",
+          body: "#{body.gsub('"', '\"')}"
+        )
+      end
+    end
+  end
 end
