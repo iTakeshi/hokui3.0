868346165bba4bac9583d3a4a4b1463de0d1f769
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-04-13 20:53:42 +0900

    Fix a bug: year without ml_account

--- app/controllers/ml_controller.rb
+++ app/controllers/ml_controller.rb
@@ -1,6 +1,11 @@
 class MlController < ApplicationController
   def index
-    @ml_archives = @current_user.year.ml_account.ml_archives.order('sent_at DESC').page(params[:page])
+    if @current_user.year.ml_account
+      @ml_archives = @current_user.year.ml_account.ml_archives.order('sent_at DESC').page(params[:page])
+    else
+      flash[:alert] = "#{@current_user.year.class_year}期のメーリングリストが登録されていません。管理者へご連絡ください。"
+      @ml_archives = []
+    end
   end
 
   def download
