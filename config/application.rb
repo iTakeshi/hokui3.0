b4e2678c41f048d9a0b8d6b26de854e2c1dac7b9
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-02-23 15:23:06 +0900

    fix fontawesome precompile

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module HokuiNet
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.precompile += ["fontawesome-webfont.ttf",
+                                 "fontawesome-webfont.eot",
+                                 "fontawesome-webfont.woff"]
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Tokyo'
